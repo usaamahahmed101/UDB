ad9b977c4db22451b6100a80cbff1a5f060ccf21
usaamahahmed101 <55158743+usaamahahmed101@users.noreply.github.com>
2022-12-12 17:24:12 -0600

    Delete credentials.tf

--- credentials.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-  project = "devops-361723"
-  # credentials = file("/Users/usaamahahmed/Desktop/GCP_DevOps_Key/DevOps/devops-361723-20bb8f647ea8.json")
-  region = "us-central1"
-}
