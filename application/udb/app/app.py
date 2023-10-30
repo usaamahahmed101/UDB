6ba0654c4e584223da5b78c6e1f53485ed655e46
usaamahahmed101 <usaamahahmed101@gmail.com>
2023-10-30 00:40:36 -0500

    new artifact

--- application/udb/app/app.py
+++ application/udb/app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('/home/usaamahahmed/application/udb/app/templates/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
