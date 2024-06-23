1bd5ebffe1a0b249342a403bc3f9190852c4fbe8
usaamahahmed101 <usaamahahmed101@gmail.com>
2024-06-22 22:39:40 -0500

    Adding app and infrastructure code for merger

--- application/udb/app/run.sh
+++
@@ -1,2 +0,0 @@
-
-gunicorn -w 1 -b 0.0.0.0:5000 app:app
--- application/udb/app/user/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from user.models import User
-from app import app
-
-@app.route('/user/signup/', methods = ['GET'])
-def get_user():
-    return User().signup_values()
